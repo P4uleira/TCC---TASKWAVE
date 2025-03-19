df2895ea7276beac8f6f62c4ddcf9044b95903e8
Paulo Ricardo <pricardoporfirio2001@gmail.com>
2025-03-19 20:38:03 -0300

    Feat: Desenvolvido o CRUD de mensagem

---
+++ TASKWAVE/TASKWAVE.API/Responses/MensagemResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace TASKWAVE.API.Responses
+{
+    public record MensagemResponse(string ConteudoMensagem, DateTime DataEnvioMensagem, int TarefaID);
+}
---
+++ TASKWAVE/TASKWAVE.DOMAIN/Services/MensagemService.cs
@@ -0,0 +1,42 @@
+﻿using TASKWAVE.API.Infrastructure.Model;
+using TASKWAVE.DOMAIN.Interfaces.Repositories;
+using TASKWAVE.DOMAIN.Interfaces.Services;
+
+namespace TASKWAVE.DOMAIN.Services
+{
+    public class MensagemService : IMensagemService
+    {
+        private readonly IMensagemRepository _messageRepository;
+
+        public MensagemService(IMensagemRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
+        public async Task CreateMessage(Mensagem message)
+        {
+            await _messageRepository.AddAsync(message);
+        }
+        
+
+        public async Task UpdateMessage(Mensagem message)
+        {
+            await _messageRepository.UpdateAsync(message);
+        }
+
+        public async Task DeleteMessage(int idMessage)
+        {
+            await _messageRepository.DeleteAsync(idMessage);
+        }
+
+        public async Task<IEnumerable<Mensagem>> GetAllMessages()
+        {
+            return await _messageRepository.GetAllAsync();
+        }
+
+        public async Task<Mensagem> GetMessageById(int idMessage)
+        {
+            return await _messageRepository.GetByIdAsync(idMessage);
+        }
+    }
+}
