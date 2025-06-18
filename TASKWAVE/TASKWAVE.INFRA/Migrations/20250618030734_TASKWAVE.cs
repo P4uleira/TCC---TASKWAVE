7e7116a7ecf94fb8f8485d3e65949e79fc93a040
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2025-06-18 01:08:43 -0300

    feat: Adição de duas colunas na TB_USUARIO para validação da troca de senha

---
+++ TASKWAVE/TASKWAVE.INFRA/Migrations/20250618030734_TASKWAVE.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASKWAVE.INFRA.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TASKWAVE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExpiracaoToken",
+                table: "TB_USUARIO",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TokenRedefinicaoSenha",
+                table: "TB_USUARIO",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataExpiracaoToken",
+                table: "TB_USUARIO");
+
+            migrationBuilder.DropColumn(
+                name: "TokenRedefinicaoSenha",
+                table: "TB_USUARIO");
+        }
+    }
+}
