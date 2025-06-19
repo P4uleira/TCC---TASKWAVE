e1c7f402902e21a0ad683f9c9b31b0116a9f698f
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2025-06-19 14:45:06 -0300

    feat: HyperLink e NavMenuOptions

---
+++ TASKWAVE/TASKWAVE.DOMAIN/Helpers/HyperLink.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASKWAVE.DOMAIN.Helpers
+{
+    public class HyperLink
+    {
+        public string Url { get; set; }
+        public string Text { get; set; }
+        public string Title { get; set; } 
+        
+        public string TypeIcon { get; set; }
+        public HyperLink(string url, string text, string title, TypeOfIcon typeIcon)
+        {
+            Url = url;
+            Text = text;
+            Title = title;
+
+            switch (typeIcon)
+            {
+                case TypeOfIcon.Home:
+                    TypeIcon = "bi-house-door-fill-nav-menu";
+                    break;
+                case TypeOfIcon.List:
+                    TypeIcon = "bi-list-nested-nav-menu";
+                    break;
+                case TypeOfIcon.Add:
+                    TypeIcon = "bi-plus-square-fill-nav-menu";
+                    break;                
+            }
+        }
+
+        public enum TypeOfIcon
+        {
+            Home,
+            Add,
+            List
+        }
+
+    }
+}
